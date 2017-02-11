62be29c7624a610a46e7f2970141aa9b91d99caa
David Li <li.davidm96@gmail.com>
2017-02-11 13:25:52 -0500

    Write CertDbSource Response method (#3)
    * Write CertDbSource Response method
    
    * Refactor CertDbSource Request method to remove nested if's
    
    * Refactor and clean comments in ocspresponder.go
    
    * Make CertDbSource Response method return response that expires latest

--- ocspresponder.go
+++ ocspresponder.go
@@ -1,6 +1,44 @@
 package main
 
-// An OCSP response source backed by certdb. See
-// cfssl.ocsp.responder.Source.
+import (
+	"encoding/hex"
+	"github.com/cloudflare/cfssl/certdb"
+	"golang.org/x/crypto/ocsp"
+)
+
+// CertDbSource is an OCSP response Source backed by certdb.
+// See the cfssl.ocsp.responder.Source interface.
 type CertDbSource struct {
+	Accessor certdb.Accessor
+}
+
+// Response implements cfssl.ocsp.responder.Source, returning the OCSP response
+// with the expiration date furthest in the future
+func (src CertDbSource) Response(req *ocsp.Request) ([]byte, bool) {
+	if req == nil {
+		return nil, false
+	}
+
+	// Extract the AKI string from req.IssuerKeyHash
+	aki := hex.EncodeToString(req.IssuerKeyHash)
+
+	sn := req.SerialNumber
+	if sn == nil {
+		return nil, false
+	}
+	strSN := sn.String()
+
+	records, err := src.Accessor.GetOCSP(strSN, aki)
+	if err == nil || len(records) == 0 {
+		return nil, false
+	}
+
+	// Find the OCSPRecord with the expiration date furthest in the future
+	cur := records[0]
+	for _, rec := range records {
+		if rec.Expiry.After(cur.Expiry) {
+			cur = rec
+		}
+	}
+	return []byte(cur.Body), true
 }
