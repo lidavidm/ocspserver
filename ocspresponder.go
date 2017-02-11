0f6d687bc41ebc4505e3c333890fe0d666c37d34
Seth <saf258@cornell.edu>
2017-02-11 11:54:59 -0500

    Refactor and clean comments in ocspresponder.go

--- ocspresponder.go
+++ ocspresponder.go
@@ -6,41 +6,32 @@ import (
 	"golang.org/x/crypto/ocsp"
 )
 
-// CertDbSource ... TODO
-// An OCSP response source backed by certdb. See
-// cfssl.ocsp.responder.Source.
-// NOTE: implements the cfssl.ocsp.responder.Source interface
+// CertDbSource is an OCSP response Source backed by certdb.
+// See the cfssl.ocsp.responder.Source interface.
 type CertDbSource struct {
-	// Write an implementation of ocsp.responder.Source that fetches an OCSP
-	// response from an instance of certdb
 	Accessor certdb.Accessor
 }
 
-// Response ... TODO
+// Response implements cfssl.ocsp.responder.Source
 func (src CertDbSource) Response(req *ocsp.Request) ([]byte, bool) {
 	if req == nil {
 		return nil, false
 	}
+
 	// Extract the AKI string from req.IssuerKeyHash
-	akiByte := make([]byte, len(req.IssuerKeyHash))
-	_, err := hex.Decode(akiByte, req.IssuerKeyHash)
-	if err == nil {
-		return nil, false
-	}
-	aki := string(akiByte)
-	var strSN string
+	aki := hex.EncodeToString(req.IssuerKeyHash)
+
 	sn := req.SerialNumber
 	if sn == nil {
 		return nil, false
 	}
-	strSN = sn.String()
+	strSN := sn.String()
+
 	record, err := src.Accessor.GetOCSP(strSN, aki)
-	if err == nil {
+	if err == nil || len(record) == 0 {
 		return nil, false
 	}
-	if len(record) == 0 {
-		return nil, false
-	}
+
 	// TODO: determine which Body
 	// field in the []... record to
 	// return
