2e2f055f447125e98b847f63d15d3fe3677b690f
David Li <li.davidm96@gmail.com>
2017-02-12 11:01:06 -0500

    Add flags to configure the OCSP signer

--- ocspserver.go
+++ ocspserver.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/cloudflare/cfssl/api/revoke"
 	"github.com/cloudflare/cfssl/certdb/dbconf"
@@ -12,16 +13,14 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func convertHandler(f http.Handler) func(http.ResponseWriter, *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		f.ServeHTTP(w, r)
-	}
-}
-
 func main() {
 	apiFlag := flag.Bool("api", false, "Run the API server.")
 	ocspFlag := flag.Bool("ocsp", false, "Run the OCSP responder.")
 	dbConfigFlag := flag.String("db-config", "", "The certdb to use.")
+	ocspIssuerFlag := flag.String("ocsp-issuer", "", "The OCSP issuer cert to use.")
+	ocspResponderFlag := flag.String("ocsp-responder", "", "The OCSP responder cert to use.")
+	ocspKeyFlag := flag.String("ocsp-responder-key", "", "The OCSP responder key to use.")
+	ocspIntervalFlag := flag.Int("oscp-interval", 60, "The OCSP response validity interval, in seconds.")
 
 	flag.Parse()
 
@@ -38,7 +37,11 @@ func main() {
 	dbAccessor := sql.NewAccessor(db)
 
 	if *apiFlag {
-		http.Handle("/api/addCert", NewHandler(dbAccessor))
+		signer, err := ocsp.NewSignerFromFile(*ocspIssuerFlag, *ocspResponderFlag, *ocspKeyFlag, time.Duration(*ocspIntervalFlag))
+		if err != nil {
+			log.Fatal("Could not create OCSP signer: ", err)
+		}
+		http.Handle("/api/addCert", NewHandler(dbAccessor, signer))
 		http.Handle("/api/revokeCert", revoke.NewHandler(dbAccessor))
 	} else {
 		handler := ocsp.NewResponder(NewSource(dbAccessor))
