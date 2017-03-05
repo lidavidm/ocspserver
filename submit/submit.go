14352208d556b3c0c1fa3fa5aec818fc8f46ae03
David Li <li.davidm96@gmail.com>
2017-03-04 21:00:01 -0500

    Add cert submitter using the test certs in Trillian repo

--- submit/submit.go
+++ submit/submit.go
@@ -2,18 +2,53 @@ package main
 
 import (
 	"context"
+	"crypto"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"io/ioutil"
 	"log"
+	"math/big"
 	"net/http"
 
 	"flag"
 
-	"github.com/cloudflare/cfssl/certdb/dbconf"
-	"github.com/cloudflare/cfssl/certdb/sql"
+	"github.com/cloudflare/cfssl/helpers"
+	ct "github.com/google/certificate-transparency/go"
 	logclient "github.com/google/certificate-transparency/go/client"
 	"github.com/google/certificate-transparency/go/jsonclient"
 
 	_ "github.com/mattn/go-sqlite3"
 )
+
+func makeCertificate(root *x509.Certificate, rootKey crypto.Signer) ([]byte, error) {
+	serialNumberRange := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberRange)
+	if err != nil {
+		return nil, err
+	}
+
+	template := x509.Certificate{
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"Cornell CS 5152"},
+		},
+		AuthorityKeyId: []byte{42, 42, 42, 42},
+	}
+	// // Generate a CA certificate
+	// issuerTemplate := x509.Certificate{
+	// 	SerialNumber: issuerSerial,
+	// 	Subject: pkix.Name{
+	// 		Organization: []string{"Cornell CS 5152"},
+	// 	},
+	// 	AuthorityKeyId: []byte{42, 42, 42, 42},
+	// 	KeyUsage:       x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+	// 	IsCA:           true,
+	// 	BasicConstraintsValid: true,
+	// }
+	return x509.CreateCertificate(rand.Reader, &template, root, rootKey.Public(), rootKey)
+}
 
 func main() {
 	var host string
@@ -28,29 +63,80 @@ func main() {
 	ctx := context.Background()
 
 	log.Print("Creating client...")
-	logclient, err := logclient.New("localhost:6962", client, options)
+	logclient, err := logclient.New("http://localhost:6962", client, options)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Print("Opening db...")
-	db, err := dbconf.DBFromConfig(dbConfig)
+	// Get the accepted roots
+	roots, err := logclient.GetAcceptedRoots(ctx)
 	if err != nil {
-		log.Print(dbConfig)
-		log.Fatal("Could not load certdb: ", err, dbConfig)
+		log.Fatal(err)
 	}
+	if roots == nil {
+		log.Fatal("No accepted roots?")
+	}
+	fmt.Println(roots)
 
-	log.Print("Creating certdb...")
-	dbAccessor := sql.NewAccessor(db)
+	// certdata, err := ioutil.ReadFile("/home/lidavidm/Code/gohome/src/github.com/google/trillian/testdata/int-ca.cert")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// certchain := testonly.CertsFromPEM(certdata)
+	// sct, err := logclient.AddChain(ctx, certchain)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(sct)
 
-	log.Print("Getting certificates...")
-	certs, err := dbAccessor.GetUnexpiredCertificates()
+	rootData, err := ioutil.ReadFile("fake-ca.cert")
+	if err != nil {
+		log.Fatal(err)
+	}
+	root, err := helpers.ParseCertificatePEM(rootData)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for _ = range certs {
-		log.Print("Submitting a cert...")
-		logclient.AddChain(ctx, nil)
+	rootKeyData, err := ioutil.ReadFile("fake-ca.privkey.pem")
+	if err != nil {
+		log.Fatal(err)
 	}
+	rootKey, err := helpers.ParsePrivateKeyPEMWithPassword(rootKeyData, []byte("gently"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cert, err := makeCertificate(root, rootKey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	sct, err := logclient.AddChain(ctx, []ct.ASN1Cert{{Data: cert}})
+	log.Println(sct)
+
+	// _, err = logclient.AddChain(ctx, []ct.ASN1Cert{{Data: root.Raw}})
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// log.Print("Opening db...")
+	// db, err := dbconf.DBFromConfig(dbConfig)
+	// if err != nil {
+	// 	log.Print(dbConfig)
+	// 	log.Fatal("Could not load certdb: ", err, dbConfig)
+	// }
+
+	// log.Print("Creating certdb...")
+	// dbAccessor := sql.NewAccessor(db)
+
+	// log.Print("Getting certificates...")
+	// certs, err := dbAccessor.GetUnexpiredCertificates()
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// for _ = range certs {
+	// 	log.Print("Submitting a cert...")
+	// 	logclient.AddChain(ctx, nil)
+	// }
 }
