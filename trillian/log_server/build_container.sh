6f6fe644d51fd029d6749a6fa2263c0152470e94
David Li <li.davidm96@gmail.com>
2017-03-13 09:20:56 -0400

    Uncomment build steps

--- trillian/log_server/build_container.sh
+++ trillian/log_server/build_container.sh
@@ -4,11 +4,11 @@ set -e
 
 DOCKERFILE_DIR=$(pwd)
 
-# go get -d -u github.com/google/trillian
-# cd $GOPATH/src/github.com/google/trillian
+go get -d -u github.com/google/trillian
+cd $GOPATH/src/github.com/google/trillian
 
-# cd server/trillian_log_server
-# CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o $DOCKERFILE_DIR/main .
+cd server/trillian_log_server
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o $DOCKERFILE_DIR/main .
 
 cd $DOCKERFILE_DIR
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o trampoline trampoline.go
